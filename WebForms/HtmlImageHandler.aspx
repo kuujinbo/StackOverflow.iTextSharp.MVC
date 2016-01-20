1345003a2a30cc63e9857983cf98efd6c4b9c8cb
kuujinbo <kuujinbo@gmail.com>
2016-01-19 21:30:45 -0800

    http://stackoverflow.com/questions/34869412 - added web forms example implementing IImageProvider for HTMLWorker

---
+++ WebForms/HtmlImageHandler.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="false" CodeBehind="HtmlImageHandler.aspx.cs" Inherits="kuujinbo.StackOverflow.iTextSharp.MVC.WebForms.HtmlImageHandler" 
+%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+<%--
+    HtmlTable used for brevity - HTML conversion for a GridView is
+    **EXACTLY** the same
+    https://msdn.microsoft.com/en-us/library/system.web.ui.htmlcontrols.htmltable.aspx
+--%>
+<table id='ConvertControlToPdf' 
+  width='100%' border='1' align='center' 
+  cellpadding='4' cellspacing='0'
+  runat='server'
+>
+<tr><td>ROW 1: CELL 1</td>
+<td><img src='/images/alt-gravatar.png' /></td></tr>
+<tr><td>ROW 2: CELL 1</td>
+<td><img src='https://en.gravatar.com/userimage/29975579/84a0f3316cb83434319696c6f33bf963.png' /></td></tr>
+<tr><td>ROW 3: CELL 1</td>
+<td><img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==' /></td></tr>
+</table>
+</div>
+<div class='tbm8'>
+<asp:Button runat='server'
+  oncommand='ProcessHtml'
+  text='Convert Html Control to PDF'
+/>
+
+
+</div> 
+</form>
+</body>
+</html>
---
+++ WebForms/HtmlImageHandler.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using iTextImage = iTextSharp.text.Image;
+
+// http://stackoverflow.com/questions/34869412
+namespace kuujinbo.StackOverflow.iTextSharp.MVC.WebForms
+{
+    public partial class HtmlImageHandler : System.Web.UI.Page
+    {
+        // ConvertControlToPdf is a HtmlTable, used for brevity.
+        // - HTML conversion for a GridView is **EXACTLY** the same
+        protected void ProcessHtml(object sender, CommandEventArgs e)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=table.pdf");
+            using (Document document = new Document())
+            {
+                PdfWriter.GetInstance(document, Response.OutputStream);
+                document.Open();
+
+                var html = new StringBuilder();
+                using (var stringWriter = new StringWriter(html))
+                {
+                    using (var htmlWriter = new HtmlTextWriter(stringWriter))
+                    {
+                        // replace with GridView control Id!
+                        ConvertControlToPdf.RenderControl(htmlWriter);
+                    }
+                }
+
+                var providers = new Dictionary<string, Object>();
+                // HTMLWorker does **NOT** understand relative URLs, so
+                // make existing ones in HTML source absolute, and handle 
+                // base64 Data URI schemes
+                var ih = new ImageHander() { BaseUri = Request.Url.ToString() };
+
+                // dictionary key 'img_provider' is **HARD-CODED**, in 
+                // iTextSharp 5.0.0 - 5.0.5, so you may need to use this
+                // interfaceProps.Add("img_provider", ih);
+                providers.Add(HTMLWorker.IMG_PROVIDER, ih);
+                //            ^^^^^^^^^^^^^^^^^^^^^^^ - constant added in 5.0.6
+                using (var sr = new StringReader(html.ToString()))
+                {
+                    foreach (IElement element in HTMLWorker.ParseToList(
+                        sr, null, providers))
+                    {
+                        PdfPTable table = element as PdfPTable;
+                        document.Add(element);
+                    }
+                }
+            }
+            Response.End();
+        }
+
+        // handle <img> tags in any System.Web.UI.Control (GridView) with:
+        // 1. base64 Data URI scheme - https://en.wikipedia.org/wiki/Data_URI_scheme
+        // 2. absolute URLs on a remote/local server 
+        // 3. relative URLs on local server (DEFAULT)
+        public class ImageHander : IImageProvider
+        {
+            public string BaseUri { get; set; }
+            public static Regex Base64 = new Regex(
+                @"^data:image/(?<mediaType>[^;]+);base64,(?<data>.*)",
+                RegexOptions.Compiled
+            );
+
+            // alias: using iTextImage = iTextSharp.text.Image;
+            public iTextImage GetImage(string src,
+                IDictionary<string, string> attrs,
+                ChainedProperties chain,
+                IDocListener doc)
+            {
+                Match match;
+                // [1]
+                if ((match = Base64.Match(src)).Length > 0)
+                {
+                    return iTextImage.GetInstance(
+                        Convert.FromBase64String(match.Groups["data"].Value)
+                    );
+                }
+
+                // [2]
+                if (!src.StartsWith("http", StringComparison.OrdinalIgnoreCase))
+                {
+                    src = HttpContext.Current.Server.MapPath(
+                        new Uri(new Uri(BaseUri), src).AbsolutePath
+                    ); 
+                }
+                return iTextImage.GetInstance(src);
+            }
+        }
+
+    }
+}
