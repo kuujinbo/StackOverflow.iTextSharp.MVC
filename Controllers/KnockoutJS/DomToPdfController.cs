1b682ac1fd22bfc32ecc742b72329f6b433fcbda
kuujinbo <kuujinbo@gmail.com>
2016-02-12 08:59:06 -0800

    http://stackoverflow.com/questions/35342874

---
+++ Controllers/KnockoutJS/DomToPdfController.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Web.Mvc;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+
+// http://stackoverflow.com/questions/35342874
+namespace kuujinbo.StackOverflow.iTextSharp.MVC.Controllers.KnockoutJS
+{
+    public class DomToPdfController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]  // some browsers have URL length limits
+        [ValidateInput(false)] // or throws HttpRequestValidationException
+        public ActionResult Index(string xHtml)
+        {
+            Response.ContentType = "application/pdf";
+            Response.AppendHeader(
+                "Content-Disposition", "attachment; filename=test.pdf"
+            );
+            using (var stringReader = new StringReader(xHtml))
+            {
+                using (Document document = new Document())
+                {
+                    PdfWriter writer = PdfWriter.GetInstance(
+                        document, Response.OutputStream
+                    );
+                    document.Open();
+                    XMLWorkerHelper.GetInstance().ParseXHtml(
+                      writer, document, stringReader
+                    );
+                }
+            }
+
+            return new EmptyResult();
+        }
+
+    }
+}
---
+++ Scripts/ajax/jquery-binary.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+    $.fn.downloadFile = function(url, data, requestType) {
+        $.ajax({
+            url: url,
+            data: data,
+            type: requestType || 'POST',
+            dataType: 'binary'
+        })
+        .done(function(data, textStatus, jqXHR) {
+            var type = jqXHR.getResponseHeader('Content-Type');
+            var filename = jqXHR.getResponseHeader('Content-Disposition');
+            filename = filename && filename.indexOf('attachment') > -1
+                ? filename.replace(/(?:[^=])+=/, '')
+                : 'file.bin';
+            var blob = new Blob([data], { type: type });
+            saveAs(blob, filename);
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            alert(errorThrown);
+        })
+        ;
+        return false;
+    };
+}(jQuery));
